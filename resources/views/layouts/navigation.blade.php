9095034ee6dc5f1184e071e4ef7f5ec07423cbcd
Topong P. <nong.tickis@gmail.com>
2026-08-12 19:45:13 +0700

    แก้ไอคอนกระดิ่ง notification ไม่แสดงบน macOS
    class h-4.5/w-4.5 ไม่ใช่ scale ที่ Tailwind รองรับ (ไม่มี CSS ถูกสร้างขึ้นเลย)
    ทำให้ SVG ไม่มีขนาดกำหนด render ต่างกันไปตามเบราว์เซอร์ เปลี่ยนเป็น h-4/w-4

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -166,7 +166,7 @@
                 <a href="{{ route('notifications.index') }}"
                    title="การแจ้งเตือน"
                    class="relative inline-flex items-center justify-center w-9 h-9 rounded-xl border border-red-900/60 bg-black/40 text-gray-400 hover:text-white hover:bg-white/[0.06] transition">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
                     </svg>
                     @if($unreadCount > 0)
